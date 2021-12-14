531fca1048fcd2367f66ffa6eb82ce2137f30306
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-14 11:29:18 -0400

    solve till 3rd task in the kata

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+class Greeter {
+  greet(name) {
+    const nameTrim = name.trim();
+    const nameCapitalized = nameTrim.charAt(0).toUpperCase() + nameTrim.slice(1);
+    return `Hello ${nameCapitalized}`;
+  }
+}
+
+
+const greeter = new Greeter()
+
+test("Greet function returns Hello <name>", () =>{
+  const resultado = greeter.greet("Maria")
+  const esperado = "Hello Maria"
+  
+  expect(resultado).toBe(esperado);
+})
+
+test("Greet function receives a name with spaces and remove the spaces in the response", () => {
+  const name = "   Maria "
+  const resultado = greeter.greet(name);
+  const esperado = `Hello ${name.trim()}`;
+
+  expect(resultado).toBe(esperado);
+})
+
+test("Greet function capitalizes the first letter of the name", () => {
+  const name = "maria"
+  const resultado = greeter.greet(name);
+  const esperado = "Hello Maria"
+
+  expect(resultado).toBe(esperado);
 });
